769107ede28ec3284e9dcfd8e694f0a50a46818e
Pavel Guranda <pguranda@1-A-16.42heilbronn.de>
2022-04-10 14:29:44 +0200

    split 1 and 3rd tests are failing

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pguranda <pguranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 10:04:33 by pguranda          #+#    #+#             */
-/*   Updated: 2022/04/10 10:47:38 by pguranda         ###   ########.fr       */
+/*   Updated: 2022/04/10 14:13:18 by pguranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ The array must end with a NULL pointer.*/
 4. abaabZeeef
 		set: abababb
 */
+
 /*#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>*/
@@ -37,29 +38,33 @@ char **ft_split(char const *s, char c)
 	int					i;
 	int					count_s;
 	int					arr_len;
+	char				*p_str;
 		
-	array_str = (char **)malloc(count_strings(s, c)*sizeof(char *) + 8);
 	count_s = 0;
 	i = 0;
 	arr_len = count_strings(s, c);
+	array_str = (char **)malloc(arr_len*sizeof(char *) + sizeof(void *));
+	if (s == NULL || array_str == NULL)
+		return (NULL);
 		while (count_s < arr_len)
 	{
-		array_str[count_s] = (char *)malloc(sizeof(char)*s_mem(s + i, c) + 1);
-		while(s[i] != c && s[i] != '\0')
+		array_str[count_s] = (char *)malloc(sizeof(char)*s_mem(s + i, c) + 1); // mem alloc
+		p_str = array_str[count_s];
+		while(s[i ] != c && s[i] != '\0')
 		{
-			*array_str[count_s] = s[i];
+			*p_str = s[i];
 			i++;
-			array_str[count_s]++;
+			p_str++;
 		}
 		if (s[i] == c)
 		{
-			*array_str[count_s] = '\0'; //maybe next?
+			*p_str = '\0'; //maybe next?
 			i++;
-			count_s++;
+			count_s++;  
 		}
 		else if (s[i] == '\0')
 		{
-			*array_str[count_s] = '\0'; 
+			*p_str = '\0'; 
 			count_s++;
 		}
 	}
@@ -93,16 +98,114 @@ int	s_mem(char const *s, char c)
 	i = 0;
 	while(s[i] != c && s[i] != '\0')
 		i++;
-	return (i);
+	if (i > 0 )
+		return (i);
+	else
+		return (0);
 }
 
-/*int				main()
-{
-	char s[20] = "obladiZoblaZda";
-	char c = 'Z';
-	char 	**array;
 
-	array = ft_split (s, c);
-	printf ("%s", array[0]);
+/*#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_print_result(char const *s)
+{
+	int		len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(1, s, len);
+}
+
+int		main(int argc, const char *argv[])
+{
+	char	**tabstr;
+	int		i;
+	int		arg;
+
+	alarm(5);
+	if (argc == 1)
+		return (0);
+	i = 0;
+	if ((arg = atoi(argv[1])) == 1)
+	{
+		if (!(tabstr = ft_split("          ", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 2)
+	{
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 3)
+	{
+		if (!(tabstr = ft_split("   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 4)
+	{
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 5)
+	{
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'z')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 6)
+	{
+		if (!(tabstr = ft_split("", 'z')))
+			ft_print_result("NULL");
+		else
+			if (!tabstr[0])
+				ft_print_result("ok\n");
+	}
 	return (0);
-}*/
+}
+*/
