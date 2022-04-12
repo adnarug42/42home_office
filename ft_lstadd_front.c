d7ad02db32856f0e9cc422233497885c55617e51
Pavel Guranda <pguranda@2-E-5.42heilbronn.de>
2022-04-12 17:56:38 +0200

    bonus

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: pguranda <pguranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 17:33:12 by pguranda          #+#    #+#             */
-/*   Updated: 2022/04/12 17:47:20 by pguranda         ###   ########.fr       */
+/*   Updated: 2022/04/12 17:56:19 by pguranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	new
+	t_list			new_node;
+	lst.next -> next = malloc(sizeof(t_list));
+	lst.next -> next = &new;
 }
