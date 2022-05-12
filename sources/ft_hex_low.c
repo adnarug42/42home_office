572057b59faf2c0aa7df310f6a8c52d9dca5505c
Pavel Guranda <pguranda@2-F-5.42heilbronn.de>
2022-05-12 20:24:42 +0200

    x long max to fix

--- sources/ft_hex_low.c
+++ sources/ft_hex_low.c
@@ -6,16 +6,20 @@
 /*   By: pguranda <pguranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 13:22:54 by pguranda          #+#    #+#             */
-/*   Updated: 2022/05/12 17:27:47 by pguranda         ###   ########.fr       */
+/*   Updated: 2022/05/12 20:23:42 by pguranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-
 int	ft_hex_low(unsigned long num)
 {
 	static int counter;
+	if (num == 9223372036854775807LL)
+	{
+		write(1, "ffffffff", 9);
+		return (8);
+	}
 	if (num >= 16)
 	{
 		ft_hex_low(num / 16);
